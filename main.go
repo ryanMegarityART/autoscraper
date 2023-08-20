f7aaf1917caee7d852fd6943552c7459bfcb263f
Ryan Megarity <ryan.megarity@immerse.io>
2023-08-20 15:58:27 +0100

    Get initial details

--- main.go
+++ main.go
@@ -10,12 +10,20 @@ import (
 )
 
 var searchPostcode string = "GU7%201EU" // gudulming station
-var searchRadius string = "100"         // miles
+var searchRadius string = "200"         // miles
 var searchMinimumModelYear string = "2020"
 var autoTraderBaseURL string = "https://www.autotrader.co.uk/"
 
+type car struct {
+	name        string
+	keySpecs    []string
+	price       string
+	detailsLink string
+}
+
 func main() {
-	scrapeAutoTrader()
+	cars := scrapeAutoTrader()
+	fmt.Println(cars)
 }
 
 func fetchHTML(url string) *http.Response {
@@ -36,7 +44,7 @@ func saveToFile(input string, filename string) {
 	}
 }
 
-func scrapeAutoTrader() {
+func scrapeAutoTrader() []car {
 	currentPage := 1
 	res := fetchHTML(autoTraderBaseURL + fmt.Sprintf("/car-search?year-from=%s&postcode=%s&radius=%s&page=%v", searchMinimumModelYear, searchPostcode, searchRadius, currentPage))
 	defer res.Body.Close()
@@ -45,15 +53,36 @@ func scrapeAutoTrader() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fullDoc, _ := goquery.OuterHtml(doc.Find("html"))
-	fmt.Printf("Full doc: %s", fullDoc)
-	saveToFile(fullDoc, "resp.html")
-	// Find the review items
-	doc.Find(".search-page__result").Each(func(i int, s *goquery.Selection) {
-		// for each print the html
-		html, _ := goquery.OuterHtml(s)
-		fmt.Printf("HTML %d: %s\n", i, html)
 
+	fullDoc, _ := goquery.OuterHtml(doc.Find("html"))
+	saveToFile(fullDoc, "resp.html")
+
+	var cars []car
+
+	// Find the review items
+	doc.Find("li[class='search-page__result']").Each(func(i int, s *goquery.Selection) {
+		carDetails := s.Find("section[class='product-card-details']")
+		carName := carDetails.Find("h3[class='product-card-details__title']")
+		var keySpecs []string
+		carDetails.Find("li").Each(func(i int, k *goquery.Selection) {
+			keySpecs = append(keySpecs, k.Text())
+		})
+
+		carPrice := s.Find("div[class='product-card-pricing__price']")
+
+		detailsLinkAnchorTag := s.Find("a[data-results-nav-fpa]")
+		detailsLink, exists := detailsLinkAnchorTag.Attr("href")
+		if exists != true {
+			log.Fatal("No link found for car")
+		}
+
+		a, _ := goquery.OuterHtml(carPrice)
+		fmt.Printf("Price %d: %s\n", i, a)
+
+		car := car{name: carName.Text(), price: carPrice.Text(), detailsLink: detailsLink, keySpecs: keySpecs}
+		cars = append(cars, car)
 	})
+
+	return cars
 
 }
